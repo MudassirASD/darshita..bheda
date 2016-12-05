caa95b3e467da1dc6a10c792c2cd7479c731be26
MudassirASD <shaikhmudassir2662@gmail.com>
2016-12-05 11:55:33 +0530

    Add files via upload

---
+++ .py
@@ -0,0 +1,9 @@
+n=int(input("Enter a number:"))
+if n>0:
+    print("The number is positive")
+elif n<0:
+      print("The number is negative:")
+else:
+       print("The number is zero")
+def n=int(input("Enter the 1st integer:"))
+      m=int(input("Enter the 2nd integer:"))
